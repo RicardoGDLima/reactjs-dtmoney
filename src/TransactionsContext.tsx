8ab0cff2e4282c93aafc1b601c9ca9e3825aee5f
Ricardo <rickgomeslima@gmail.com>
2021-10-02 09:45:13 -0300

    Finalizando Inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,3 +1,4 @@
+import { promises } from 'fs';
 import { createContext, useEffect, useState, ReactNode } from 'react'
 import { api } from './services/api';
 
@@ -18,12 +19,12 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 
 export const TransactionsContext = createContext<TransactionsContextData>(
-   {} as TransactionsContextData
+    {} as TransactionsContextData
 );
 
 
@@ -35,12 +36,21 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, [])
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        })
+        const { transaction } = response.data
+
+        setTransactions([
+            ...transactions,
+            transaction
+        ])
     }
 
     return (
-        <TransactionsContext.Provider value={{transactions, createTransaction}}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     );
